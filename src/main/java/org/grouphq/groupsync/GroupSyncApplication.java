77ee59d2108c9cb3d7074136dcd9200e4d571757
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2023-09-19 05:54:26 -0400

    Initial state

---
+++ src/main/java/org/grouphq/groupsync/GroupSyncApplication.java
@@ -0,0 +1,13 @@
+package org.grouphq.groupsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/grouphq/groupsync/GroupSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grouphq.groupsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
