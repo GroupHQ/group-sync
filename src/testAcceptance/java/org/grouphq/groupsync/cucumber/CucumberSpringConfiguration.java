2a81d715ceb8dead895d6d821c112f2ed33a4fd3
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2023-10-03 20:09:20 -0400

    GROUP-16 Updated configuration

--- src/testAcceptance/java/org/grouphq/groupsync/cucumber/CucumberSpringConfiguration.java
+++ src/testAcceptance/java/org/grouphq/groupsync/cucumber/CucumberSpringConfiguration.java
@@ -3,12 +3,17 @@ package org.grouphq.groupsync.cucumber;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.TestPropertySource;
 
 /**
  * Configuration for linking Cucumber with the Spring context.
  */
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Profile("production")
+@TestPropertySource(properties =
+    {"grouphq.group-service.url=http://localhost:9001", "spring.cloud.config.fail-fast=true"})
 @AutoConfigureWebTestClient
 public class CucumberSpringConfiguration {
 }
