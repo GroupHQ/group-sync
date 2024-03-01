be6d4712e45f4800138b5dd100f392159bf667c7
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2024-02-29 21:04:54 -0500

    GROUP-89 Update models to ignore @type annotation

--- src/main/java/org/grouphq/groupsync/groupservice/domain/outbox/ErrorData.java
+++ src/main/java/org/grouphq/groupsync/groupservice/domain/outbox/ErrorData.java
@@ -1,10 +1,14 @@
 package org.grouphq.groupsync.groupservice.domain.outbox;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Data class for the error field in the outbox.
+ * The @type annotation is temporarily being ignored until GROUP-89 is resolved.
  *
  * @param error The error message.
  */
+@JsonIgnoreProperties(value = {"@type"})
 public record ErrorData(
     String error
 ) {
